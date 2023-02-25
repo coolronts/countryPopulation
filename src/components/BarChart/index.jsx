83a354e10e47530ce4084e574d2d253f8b8dc195
Zahid Ronty <230759@usn.no>
2023-02-25 11:01:33 +0100

    more fixes

--- src/components/BarChart/index.jsx
+++ src/components/BarChart/index.jsx
@@ -7,32 +7,37 @@ export default function BarChart({ allData }) {
   /* -------------------------------------------------------------------------- */
   const [data, setData] = useState([]);
   const [year, setYear] = useState(null);
+  const [isDisabled, setIsDisabled] = useState(false);
   
   /* -------------------------------------------------------------------------- */
   /*                                     CSS                                    */
   /* -------------------------------------------------------------------------- */
   const styles = {
     container: "flex justify-center items-center",
-    year: "text-7xl text-slate-500"
+    year: "text-7xl text-slate-500",
+    button: "ml-12 bg-slate-500 text-white px-4 py-2 rounded hover:bg-slate-600 transition duration-200 ease-in-out cursor-pointer",
+    disabledButton: "ml-12 bg-slate-500 text-white px-4 py-2 rounded opacity-50 cursor-not-allowed"
   }
   /* -------------------------------------------------------------------------- */
   /*                                   METHODS                                  */
   /* -------------------------------------------------------------------------- */
   const Replay = () => {
+    setIsDisabled(true);
     const keys = allData.keys();
     let i = 0;
     const intervalId = setTimeout(function nextYear() {
       const currentKey = keys.next().value;
       if (!currentKey) {
         clearTimeout(intervalId);
+        setIsDisabled(false);
         return;
       }
       setData(allData.get(currentKey));
       setYear(currentKey);
       i++;
       if (i < allData.size)
-        setTimeout(nextYear, 2000);
-    }, 1000);
+        setTimeout(nextYear, 500);
+    }, 500);
     return () => clearTimeout(intervalId);
   }
   /* -------------------------------------------------------------------------- */
@@ -60,18 +65,25 @@ export default function BarChart({ allData }) {
   /*                                     JSX                                    */
   /* -------------------------------------------------------------------------- */
   return (
+  
     <div className={styles.container}>
-        <button onClick={() => Replay()}>Log Data</button>
-        <ChartRace data={data} 
-            backgroundColor='#fff'
-            width={760}
-            padding={1}
-            itemHeight={24}
-            gap={1}
-            titleStyle={{ font: 'normal 400 24px Arial', color: '#f2f' }}
-            valueStyle={{ font: 'normal 400 16px Arial', color: 'rgba(120,120,120)' }}
-      />
-      <p className={styles.year}>{year}</p>
+      {data.length === 0 ? <p>Loading</p> :
+        <>
+          <ChartRace data={data} 
+              backgroundColor='#fff'
+              width={760}
+              padding={1}
+              itemHeight={24}
+              gap={1}
+              titleStyle={{ font: 'normal 400 24px Arial', color: '#f2f' }}
+              valueStyle={{ font: 'normal 400 16px Arial', color: 'rgba(120,120,120)' }}
+          />
+          <div className="flex items-center">
+            <p className={styles.year}>{year}</p> 
+            <button className={isDisabled? styles.disabledButton : styles.button} onClick={() => Replay()} >Replay</button>
+            </div>
+          </>
+      }
       </div>
     );
   }
